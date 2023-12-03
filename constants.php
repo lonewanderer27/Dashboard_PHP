d3d27b929263a4115e69d54218a5c010c1eccf1d
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-12-04 06:54:20 +0800

    This commit doesn't match the provided diff. Here's the correct commit message:
    "Update admin user details in constants.php"
    
    Admin user details have been updated in 'constants.php' file. The name, username, email, phone, and address have been changed for security and consistency purposes.

--- constants.php
+++ constants.php
@@ -12,9 +12,9 @@ $USER = "user";
 $ADMIN = "admin";
 $FULLNAME = "fullname";
 
-$AD_FULLNAME = "Zyrich Kio";
-$AD_USERNAME = "admin";
-$AD_EMAIL = "zyrich.kio.salonga@adamson.edu.ph";
+$AD_FULLNAME = "Kimberly Rei Delacruz";
+$AD_USERNAME = "kimrei";
+$AD_EMAIL = "kimberly.rei.dela.cruz@adamson.edu.ph";
 $AD_PASSWORD = "admin";
-$AD_PHONE = "(123) 456-7890";
-$AD_ADDRESS = "1234 Rizal Avenue, Barangay 56, Manila";
+$AD_PHONE = "+63 905 123 4567";
+$AD_ADDRESS = "123 Barangay Street, Makati City, Metro Manila";
