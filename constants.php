3d847a0d781f718f76fe2e9922033237db72f7de
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-12-04 05:38:49 +0800

    Update admin account details in constants.php
    Admin account details have been updated to provide better site security and accuracy. This is reflected in the constants.php file where $AD_FULLNAME, $AD_USERNAME, and $AD_EMAIL values have been changed, replacing old values with new one's respective to Zyrich Kio(admin).

--- constants.php
+++ constants.php
@@ -12,9 +12,9 @@ $USER = "user";
 $ADMIN = "admin";
 $FULLNAME = "fullname";
 
-$AD_FULLNAME = "John Andrei";
-$AD_USERNAME = "andrei";
-$AD_EMAIL = "cabilijohnandrei@gmail.com";
+$AD_FULLNAME = "Zyrich Kio";
+$AD_USERNAME = "admin";
+$AD_EMAIL = "zyrich.kio.salonga@adamson.edu.ph";
 $AD_PASSWORD = "admin";
 $AD_PHONE = "(123) 456-7890";
 $AD_ADDRESS = "1234 Rizal Avenue, Barangay 56, Manila";
